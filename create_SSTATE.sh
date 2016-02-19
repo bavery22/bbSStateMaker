21d4fdb60f98c921f824dea7793323a88bcb0f92
bavery <brian.avery@intel.com>
2016-02-19 14:45:40 -0800

    add build

--- create_SSTATE.sh
+++ create_SSTATE.sh
@@ -1,31 +1,69 @@
 #!/bin/bash
 
+TOPDIR=`pwd`
+DOWNLOADS="/big/DL_DIR"
 usage()
 {
-  echo "Usage:$0 <BRANCH> <MACHINE>"
+  echo "Usage:$0 <BRANCH> <MACHINE> TARGETS"
   exit 1
 }
+
+POKY="http://git.yoctoproject.org/git/poky"
+
+
+
 clone_it ()
 {
-    mkdir -p $BRANCH
-    ls -al $BRANCH
 
+    mkdir -p $TOPDIR/SRC/$BRANCH
+    cd $TOPDIR/SRC/$BRANCH
+    if [ ! -d poky ]; then
+	git clone $POKY
+    fi
+    cd poky
+    git fetch --all
+    git reset --hard HEAD
+    git checkout origin/$BRANCH
+    git branch -D $BRANCH
+    git checkout -b $BRANCH origin/$BRANCH
+    git clean -fdx
 }
 
+build_it ()
+{
+    cd $TOPDIR/SRC/$BRANCH
+    rm -rf build
+    source ./poky/oe-init-build-env
+    echo ' I am in ' `pwd`
+    echo "DL_DIR=\"$DOWNLOADS\" ">> conf/local.conf
+    echo "MACHINE=\"$MACHINE\" ">> conf/local.conf
+    for t in $TARGETS; do
+	echo "ME BUILD $t"
+	time bitbake $t
+    done
+
+}
 
 if [ "$1" = ""  ]; then
     usage;
 else
     BRANCH=$1;
 fi
-
-if [ "$2" = ""  ]; then
+shift
+if [ "$1" = ""  ]; then
     usage;
 else
-    MACHINE=$2;
+    MACHINE=$1;
+fi
+shift
+TARGETS=$*
+if [ "$TARGETS" = ""  ]; then
+    usage;
 fi
 
 echo "BRANCH=$BRANCH"
-
+echo "MACHINE=$MACHINE"
+echo "TARGETS=$TARGETS"
 
 clone_it
+build_it
