10362d696e03990ddeeeac66c4101beb0f12b66c
bavery <brian.avery@intel.com>
2016-03-04 13:53:53 -0800

    turned branch,machines, and images into lists to work through.

--- pushToDownloads.sh
+++ pushToDownloads.sh
@@ -2,16 +2,34 @@
 
 
 STARTTIME=$(date +%s)
-
-time ./create_SSTATE.sh master qemux86-64 core-image-minimal core-image-sato ;
-time ./create_SSTATE.sh master qemux86 core-image-minimal core-image-sato;
-echo "Copying to Staging Area"
-rsync -a SRC/master/sstate-cache/ OUT/master/sstate-cache/;
-echo "Copying to Remote"
-rsync -a OUT/ bavery@downloads.yoctoproject.org:/sstate-test/;
-ENDTIME=$(date +%s)
-DELTATIME=$(($ENDTIME - $STARTTIME))
-echo "It takes $DELTATIME seconds to complete this task..."
+BRANCHES="master"
+MACHINES="qemux86-64 qemux86"
+IMAGES="core-image-minimal core-image-sato"
+for BRANCH in $BRANCHES; do
+    for MACHINE in $MACHINES; do
+	for IMAGE in $IMAGES; do
+	    echo "Working on $BRANCH for $MACHINE and $IMAGE image"
+	    time ./create_SSTATE.sh ${BRANCH} ${MACHINE} ${IMAGE} ;
+	    echo "Cleaning Staging area of files more than 2 days old."
+	    NUM_FILES_REMOVED=`find SRC/${BRANCH}/sstate-cache/ -mtime +2 | wc -l `
+	    echo "Cleaning ${NUM_FILES_REMOVED} files"
+	    find SRC/${BRANCH}/sstate-cache/ -mtime +2 -exec rm -rf {} \;
+	    echo "Final Size: "`du -sh SRC/${BRANCH}/sstate-cache/`
+	    echo "Copying to Staging Area"
+	    mkdir -p OUT/${BRANCH}/sstate-cache/
+	    rsync -a --delete SRC/${BRANCH}/sstate-cache/ OUT/${BRANCH}/sstate-cache/;
+	    echo "Copying to Remote"
+	    rsync -a --delete OUT/ bavery@downloads.yoctoproject.org:/sstate-test/;
+	done
+    done
+done
+	ENDTIME=$(date +%s)
+	DELTATIME=$(($ENDTIME - $STARTTIME))
+	DELTAMIN=$(($DELTATIME/60))
+	echo "We did Branches=$BRANCHES"
+	echo "We did Machines=$MACHINES"
+	echo "We did Images=$IMAGES"
+	echo "It took $DELTATIME seconds ($DELTAMIN minutes) to complete this task..."
 #if [ $DELTATIME -gt 3600 ]; then
 #    SLEEPTIME=$((3600 - $DELTATIME))
 #    echo "sleeping for $SLEEPTIME secs..."
