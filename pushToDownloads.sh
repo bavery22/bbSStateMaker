fe70c2503d0733474685303121bc0742b0177861
bavery <brian.avery@intel.com>
2016-03-08 17:44:37 -0800

    added times and manifest file

--- pushToDownloads.sh
+++ pushToDownloads.sh
@@ -2,10 +2,17 @@
 
 
 STARTTIME=$(date +%s)
-BRANCHES="master"
+BRANCHES="master jethro"
 MACHINES="qemux86-64 qemux86"
 IMAGES="core-image-minimal core-image-sato"
+MANIFEST="MANIFEST.txt"
 for BRANCH in $BRANCHES; do
+    rm -f SRC/${BRANCH}/$MANIFEST
+    date | tee SRC/${BRANCH}/$MANIFEST
+    (cd SRC/${BRANCH}/poky;git log | head -1) >> SRC/${BRANCH}/$MANIFEST
+    echo "branch=$BRANCH" >> SRC/${BRANCH}/$MANIFEST
+    echo "machines=$MACHINES" >> SRC/${BRANCH}/$MANIFEST
+    echo "images=$IMAGES" >> SRC/${BRANCH}/$MANIFEST
     for MACHINE in $MACHINES; do
 	for IMAGE in $IMAGES; do
 	    echo "Working on $BRANCH for $MACHINE and $IMAGE image"
@@ -18,18 +25,20 @@ for BRANCH in $BRANCHES; do
 	    echo "Copying to Staging Area"
 	    mkdir -p OUT/${BRANCH}/sstate-cache/
 	    rsync -a --delete SRC/${BRANCH}/sstate-cache/ OUT/${BRANCH}/sstate-cache/;
-	    echo "Copying to Remote"
-	    rsync -a --delete OUT/ bavery@downloads.yoctoproject.org:/sstate-test/;
+	    rsync -a --delete SRC/${BRANCH}/${MANIFEST} OUT/${BRANCH}/${MANIFEST};
 	done
     done
 done
-	ENDTIME=$(date +%s)
-	DELTATIME=$(($ENDTIME - $STARTTIME))
-	DELTAMIN=$(($DELTATIME/60))
-	echo "We did Branches=$BRANCHES"
-	echo "We did Machines=$MACHINES"
-	echo "We did Images=$IMAGES"
-	echo "It took $DELTATIME seconds ($DELTAMIN minutes) to complete this task..."
+echo "Copying to Remote"
+rsync -a --delete OUT/ bavery@downloads.yoctoproject.org:/sstate-test/
+
+ENDTIME=$(date +%s)
+DELTATIME=$(($ENDTIME - $STARTTIME))
+DELTAMIN=$(($DELTATIME/60))
+echo "We did Branches=$BRANCHES"
+echo "We did Machines=$MACHINES"
+echo "We did Images=$IMAGES"
+echo "It took $DELTATIME seconds ($DELTAMIN minutes) to complete this task..."
 #if [ $DELTATIME -gt 3600 ]; then
 #    SLEEPTIME=$((3600 - $DELTATIME))
 #    echo "sleeping for $SLEEPTIME secs..."
